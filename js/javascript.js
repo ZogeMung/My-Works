d8e9e17c307d8ffea06f999317e01c37a31aa354
ZogeMung <1533567830@qq.com>
2021-09-26 13:27:08 +0800

    Use vue3.0.

---
+++ js/javascript.js
@@ -0,0 +1,39 @@
+function vuestart() {
+    Vue.createApp({
+        data() {
+            return {
+                inl: './works/',
+                outl: '.mp3',
+                items: [{
+                        title: 'Never Let You Go',
+                        filename: 'NeverLetYouGo'
+                    },
+                    {
+                        title: '冬日里的一棵树',
+                        filename: '冬日里的一棵树'
+                    },
+                    {
+                        title: '时间缝隙',
+                        filename: '时间缝隙'
+                    },
+                    {
+                        title: '那么奇怪（马蒂尼）',
+                        filename: '那么奇怪'
+                    },
+                    {
+                        title: '你不在北京',
+                        filename: '你不在北京'
+                    },
+                    {
+                        title: '醒',
+                        filename: '醒'
+                    },
+                    {
+                        title: '何须问',
+                        filename: '何须问'
+                    }
+                ]
+            }
+        }
+    }).mount('#files')
+}
