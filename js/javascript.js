ea825e5cee9d8d71f56a9304e0de6dc37f58c8ec
ZogeMung <1533567830@qq.com>
2023-02-01 20:53:01 +0800

    Add 陪你到爱的边境.mp3

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,11 @@ function vuestart() {
                 inl: 'https://cdn.jsdelivr.net/gh/ZogeMung/My-Works/works/',
                 outl: '.mp3',
                 items: [{
+                        artist: '半生瓜/皮特',
+                        title: '陪你到爱的边境',
+                        filename: '陪你到爱的边境.mp3'
+                    },
+                    {
                         artist: '皮特/半生瓜',
                         title: '盛世',
                         filename: '盛世.mp3'
